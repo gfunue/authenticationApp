1337f8cfb3f67003747b6630b6d311a2d3db4378
gfunue <fenue.genjang@gmail.com>
2024-01-25 16:06:58 -0500

    Initial project configuration.

---
+++ src/main/java/com/secureauthenticationapp/authenticationapp/AuthenticationappApplication.java
@@ -0,0 +1,13 @@
+package com.secureauthenticationapp.authenticationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secureauthenticationapp/authenticationapp/AuthenticationappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureauthenticationapp.authenticationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
