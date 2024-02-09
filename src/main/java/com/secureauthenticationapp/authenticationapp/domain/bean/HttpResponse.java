10aa89cda1cbb1734de2fd4c104cf617cb2439f4
Edwin <fenue.genjang@gmail.com>
2024-02-09 17:28:31 -0600

    Add HttpResponse bean
    This commit introduces a new HttpResponse Java bean. This bean is designed to standardize the format of HTTP responses across the application, making it easier to encode and decode these responses. It includes fields for the timestamp, status code, status, reason, messages, and additional data or response list.

---
+++ src/main/java/com/secureauthenticationapp/authenticationapp/domain/bean/HttpResponse.java
@@ -0,0 +1,26 @@
+package com.secureauthenticationapp.authenticationapp.domain.bean;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
+
+@Data
+@SuperBuilder
+@JsonInclude(NON_DEFAULT)
+public class HttpResponse {
+    protected String timeStamp;
+    protected int statusCode;
+    protected HttpStatus status;
+    protected String reason;
+    protected String message;
+    protected String developerMessage;
+    protected Map<?, ?> data;
+    protected List<?> respondList;
+}
+
