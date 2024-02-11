af70a07641f909b6c0f2aeec41c1f5dec551c369
Edwin <fenue.genjang@gmail.com>
2024-02-10 20:45:53 -0600

    Add new Exception classes and handlers
    Added new Exception classes: TokenValidationException, BlogNotFoundException, and BlogOperationException. Also, updated the GlobalExceptionHandler to include handlers for these new exceptions, handle ConstraintViolationException, remove the general exception handler, and modify some existing handlers' response status.

---
+++ src/main/java/com/secureauthenticationapp/authenticationapp/domain/exception/TokenValidationException.java
@@ -0,0 +1,9 @@
+package com.secureauthenticationapp.authenticationapp.domain.exception;
+
+public class TokenValidationException extends RuntimeException {
+    public TokenValidationException(String message) {
+        super(message);
+    }
+
+}
+
